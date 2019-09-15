6e0e9c0b66ec98407d889c19c43403bf4743181d
theDeepVoice <2859826534@qq.com>
2019-09-15 13:29:21 +0800

    2019.9.15

---
+++ Work/中秋作业/Long.java
@@ -0,0 +1,12 @@
+package first_homework;
+
+public class Long {
+	public static void main(String[] args) {
+		double x=5;
+		for(int i=0; i<5; i++) {
+			x = x/2.0;
+		}
+		System.out.println(x);
+	}
+
+}
